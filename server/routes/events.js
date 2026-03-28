7ce64fa2d19ce4778f2231d083a94a59df26cbf3
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-03-28 23:07:22 +0530

    Added event routes

---
+++ server/routes/events.js
@@ -0,0 +1,23 @@
+const express= require('express');
+const router= express.Router();
+const {protect, admin}= require('../middleware/auth');
+const {} = require('../controllers/eventControllers');
+
+//Get all events
+router.get('/', getAllEvents);
+
+//Get event by Id
+router.get('/:id', getEventById);
+
+//Create evet (Admin Only)
+router.post('/', protect, admin, createEvent);
+
+
+//Update event (Admin Only)
+router.put('/:id', protect, admin, updateEvent);
+
+//Delete event (Admin Only)
+router.delete('/:id', protect, admin, deleteEvent);
+
+
+module.exports= router;
