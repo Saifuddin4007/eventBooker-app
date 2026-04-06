f22ec233204035f3e22e9149846d1a7845c2ccb5
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-04-06 20:01:38 +0530

    Updated cors part

--- server/index.js
+++ server/index.js
@@ -3,14 +3,24 @@ const cors= require('cors');
 const dotenv= require('dotenv');
 const mongoDB= require('./db/db');
 const authRoutes= require('./routes/auth');
+const eventRoutes= require('./routes/events');
+const bookingRoutes= require('./routes/bookings');
 
 dotenv.config();
 const app= express();
 const PORT= process.env.PORT || 5000;
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173', // your React app's exact origin
+    credentials: true,               // required because axios has withCredentials: true
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 //!Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/bookings', bookingRoutes);
 
 
 //!Connect to DB
