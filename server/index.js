88ba96f14a4d268592b00d7c8fe5e3221c486acc
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-04-06 21:56:10 +0530

    Update build

--- server/index.js
+++ server/index.js
@@ -5,10 +5,17 @@ const mongoDB= require('./db/db');
 const authRoutes= require('./routes/auth');
 const eventRoutes= require('./routes/events');
 const bookingRoutes= require('./routes/bookings');
+const path= require('path');
+
+const _dirname= path.resolve();
+
+
+//!Connect to DB
+mongoDB();
 
 dotenv.config();
 const app= express();
-const PORT= process.env.PORT || 5000;
+const PORT= process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5173', // your React app's exact origin
@@ -22,12 +29,16 @@ app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/bookings', bookingRoutes);
 
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.use((req,res)=>{
+    res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+})
 
-//!Connect to DB
-mongoDB();
 
-app.use(cors());
+
 app.use(express.json());
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
